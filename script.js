4b451b47bb9e06f5ae5f8d04266f39048ae142ed
Zé Elias <elias.jpaiva@gmail.com>
2022-06-24 16:06:06 +0200

    javascript part almost completed.

--- script.js
+++ script.js
@@ -3,8 +3,25 @@ const inputTopLeft = document.querySelector('.border-radius-top-left');
 const inputTopRight = document.querySelector('.border-radius-top-right');
 const inputBottomLeft = document.querySelector('.border-radius-bottom-left');
 const inputBottomRight = document.querySelector('.border-radius-bottom-right');
+const border = document.querySelector('.inside-boder');
 
+// Event Listeners
 inputTopLeft.addEventListener('input', ()=> {
-  console.log(inputTopLeft.value); 
-  // inputTopLeft.style.borderTopLeftRadius = topLeftValue;
+  const valueTopLeft = Number(inputTopLeft.value);
+  border.style.borderTopLeftRadius = `${valueTopLeft}px`;
 })
+
+inputTopRight.addEventListener('input', () => {
+  const valueTopRight = Number(inputTopRight.value);
+  border.style.borderTopRightRadius = `${valueTopRight}px`;
+});
+
+inputBottomLeft.addEventListener('input', () => {
+  const valueBottomLeft = Number(inputBottomLeft.value);
+  border.style.borderBottomLeftRadius = `${valueBottomLeft}px`;
+});
+
+inputBottomRight.addEventListener('input', () => {
+  const valueBottomRight = Number(inputBottomRight.value);
+  border.style.borderBottomRightRadius = `${valueBottomRight}px`;
+});
