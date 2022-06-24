8485fbc682ab9e300650a05e6b53647b747a5db0
Zé Elias <elias.jpaiva@gmail.com>
2022-06-24 16:20:27 +0200

    added input and button, so users can copy border-radius value

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const inputTopRight = document.querySelector('.border-radius-top-right');
 const inputBottomLeft = document.querySelector('.border-radius-bottom-left');
 const inputBottomRight = document.querySelector('.border-radius-bottom-right');
 const border = document.querySelector('.inside-boder');
+const copyInput = document.querySelector('.input-copy');
 
 // Event Listeners
 inputTopLeft.addEventListener('input', ()=> {
