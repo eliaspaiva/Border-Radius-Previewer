0ebb130bcbd7ced3a152e456ac6a4674cabe7ac1
Zé Elias <elias.jpaiva@gmail.com>
2022-06-25 15:01:17 +0200

    in progress

--- script.js
+++ script.js
@@ -5,26 +5,31 @@ const inputBottomLeft = document.querySelector('.border-radius-bottom-left');
 const inputBottomRight = document.querySelector('.border-radius-bottom-right');
 const border = document.querySelector('.inside-boder');
 const copyInput = document.querySelector('.input-copy');
-  
+const emptyArray = [];
+
 
 // Event Listeners
-inputTopLeft.addEventListener('input', ()=> {
+inputTopLeft.addEventListener('input', function() {
   const valueTopLeft = Number(inputTopLeft.value);
   border.style.borderTopLeftRadius = `${valueTopLeft}px`;
-  copyInput.value = valueTopLeft;
+  emptyArray.push(valueTopLeft);
 })
 
 inputTopRight.addEventListener('input', () => {
   const valueTopRight = Number(inputTopRight.value);
   border.style.borderTopRightRadius = `${valueTopRight}px`;
+  emptyArray.push(valueTopRight);
 });
 
 inputBottomLeft.addEventListener('input', () => {
   const valueBottomLeft = Number(inputBottomLeft.value);
   border.style.borderBottomLeftRadius = `${valueBottomLeft}px`;
+  emptyArray.push(valueBottomLeft);
 });
 
 inputBottomRight.addEventListener('input', () => {
   const valueBottomRight = Number(inputBottomRight.value);
   border.style.borderBottomRightRadius = `${valueBottomRight}px`;
+  emptyArray.push(valueBottomRight);
+  console.log(emptyArray);
 });
