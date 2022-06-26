662f0b48b75069487e30d789ec77bdd108c9464e
Zé Elias <elias.jpaiva@gmail.com>
2022-06-26 08:58:40 +0200

    in progress

--- script.js
+++ script.js
@@ -5,36 +5,37 @@ const inputBottomLeft = document.querySelector('.border-radius-bottom-left');
 const inputBottomRight = document.querySelector('.border-radius-bottom-right');
 const border = document.querySelector('.inside-boder');
 const copyInput = document.querySelector('.input-copy');
-const teste = [];
+let valueLeftTop = 0;
+let valueRightTop = 0;
+let valueLeftBottom = 0;
+let valueRightBottom = 0;
 
 // Event Listeners
 inputTopLeft.addEventListener('input', () => {
   const valueTopLeft = Number(inputTopLeft.value);
   border.style.borderTopLeftRadius = `${valueTopLeft}px`;
-  teste.push(valueTopLeft);
+  valueLeftTop = valueTopLeft;
+  copyInput.value = `${valueLeftTop}px`;
 });
 
 inputTopRight.addEventListener('input', () => {
   const valueTopRight = Number(inputTopRight.value);
   border.style.borderTopRightRadius = `${valueTopRight}px`;
-  teste.push(valueTopRight);  
+  valueRightTop = valueTopRight;
+  copyInput.value = `${valueLeftTop}px ${valueRightTop}px`;
 });
 
 inputBottomLeft.addEventListener('input', () => {
   const valueBottomLeft = Number(inputBottomLeft.value);
   border.style.borderBottomLeftRadius = `${valueBottomLeft}px`;
-  teste.push(valueBottomLeft);
+  valueLeftBottom = valueBottomLeft;
+  copyInput.value = `${valueLeftTop}px ${valueRightTop}px ${valueLeftBottom}px`;
 });
 
 inputBottomRight.addEventListener('input', () => {
   const valueBottomRight = Number(inputBottomRight.value);
   border.style.borderBottomRightRadius = `${valueBottomRight}px`;
-  teste.push(valueBottomRight);
-  console.log(valueBottomRight);
-  for (let index = 0; index < teste.length; index++) {
-    const element = teste[index];
-    console.log(element);
-    copyInput.value = `${teste[0]}px ${teste[1]}px ${teste[2]}px ${teste[3]}px`;
-  }
+  valueRightBottom = valueBottomRight;
+  copyInput.value = `${valueLeftTop}px ${valueRightTop}px ${valueLeftBottom}px ${valueRightBottom}px`;
   }
 ); 
