0337d54bf5c483bb8b202a981c5e1d2ef0ba0fec
Zé Elias <elias.jpaiva@gmail.com>
2022-06-26 08:26:02 +0200

    in progress

--- script.js
+++ script.js
@@ -5,50 +5,36 @@ const inputBottomLeft = document.querySelector('.border-radius-bottom-left');
 const inputBottomRight = document.querySelector('.border-radius-bottom-right');
 const border = document.querySelector('.inside-boder');
 const copyInput = document.querySelector('.input-copy');
-const emptyArray = [];
 const teste = [];
-
 
 // Event Listeners
 inputTopLeft.addEventListener('input', () => {
   const valueTopLeft = Number(inputTopLeft.value);
   border.style.borderTopLeftRadius = `${valueTopLeft}px`;
-  emptyArray.push(valueTopLeft);
-  for (let index = 0; index < emptyArray.length; index++) {
-    const element = emptyArray[index];
-    if (element == valueTopLeft) teste.push(element);
-  }
+  teste.push(valueTopLeft);
 });
 
 inputTopRight.addEventListener('input', () => {
   const valueTopRight = Number(inputTopRight.value);
   border.style.borderTopRightRadius = `${valueTopRight}px`;
-  emptyArray.push(valueTopRight);
-  for (let index = 0; index < emptyArray.length; index++) {
-    const element = emptyArray[index];
-    if (element == valueTopRight) teste.push(element);
-  }
+  teste.push(valueTopRight);  
 });
 
 inputBottomLeft.addEventListener('input', () => {
   const valueBottomLeft = Number(inputBottomLeft.value);
   border.style.borderBottomLeftRadius = `${valueBottomLeft}px`;
-  emptyArray.push(valueBottomLeft);
-  for (let index = 0; index < emptyArray.length; index++) {
-    const element = emptyArray[index];
-    if (element === valueBottomLeft) teste.push(element);
-  }
+  teste.push(valueBottomLeft);
 });
 
 inputBottomRight.addEventListener('input', () => {
   const valueBottomRight = Number(inputBottomRight.value);
   border.style.borderBottomRightRadius = `${valueBottomRight}px`;
-  emptyArray.push(valueBottomRight);
-  console.log(emptyArray);
+  teste.push(valueBottomRight);
   console.log(valueBottomRight);
-  for (let index = 0; index < emptyArray.length; index++) {
-    const element = emptyArray[index];
-    if (element == valueBottomRight) teste.push(element);
-    console.log(teste);
+  for (let index = 0; index < teste.length; index++) {
+    const element = teste[index];
+    console.log(element);
+    copyInput.value = `${teste[0]}px ${teste[1]}px ${teste[2]}px ${teste[3]}px`;
   }
-}); 
+  }
+); 
